14279853606240eaa1414e6d033f7f83535862db
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-04-27 23:34:50 +0500

    #1

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+import core.views
+
+
+app_name = 'core'
+
+
+urlpatterns = [
+    # path('appleal/', core.views.appleal, name='appleal'),
+    # path('declarer_phone/<int:pk>/', core.views.declarer_phone, name='declarer_phone'),
+    # path('no_admin/', core.views.no_admin, name='no_admin'),
+    # path('val/<str:values>', core.views.val, name='val'),
+    # path('serp/<str:values>', core.views.serp, name='serp'),
+    # path('declarer_js/<int:pk>', core.views.declarer_js, name='declarer_js'),
+    path('appleals/<int:pk>', core.views.appeal_detail),
+    path('declarers/<int:pk>', core.views.declarer_detail),
+    path('services/<int:pk>', core.views.service_detail),
+    path('appeal_list/', core.views.appeal_list)
+]
